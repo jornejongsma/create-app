f0cbc45a7c5c978865e2b89de5d4c45a1e28d4cb
Jorne Envy <jornejongsma@gmail.com>
2021-12-06 12:20:37 +0100

    bin

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+
+function runCommand(command) {
+  try {
+    execSync(`${command}`, {stdio: 'inherit'});
+  } catch(error) {
+    console.error(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+}
+
+const repoName = process.argv[2];
+const githubRepo = ``;
+const gitCheckoutCommand = `git clone --depth 1 ${githubRepo} ${repoName}`;
+const installDepthCommand = `cd ${repoName} && yarn install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit({code: -1});
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepthCommand);
+if(!installedDeps) process.exit({code: -1});
+
+console.log("Congratulations! You are ready. Follow the following commands to start");
+console.log(`cd ${repoName} && npm start`);
