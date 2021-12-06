65b3992beeb796b7e8d37126fcdaf374597834db
Jorne Envy <jornejongsma@gmail.com>
2021-12-06 12:43:06 +0100

    public

--- bin/cli.js
+++ bin/cli.js
@@ -13,6 +13,8 @@ function runCommand(command) {
 }
 
 const repoName = process.argv[2];
+
+// This link should be public
 const githubRepo = `https://github.com/jornejongsma/create-app`;
 const gitCheckoutCommand = `git clone --depth 1 ${githubRepo} ${repoName}`;
 const installDepthCommand = `cd ${repoName} && yarn install`;
