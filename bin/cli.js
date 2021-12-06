add9746afbc98b53e3640db76ade9e50fc6179ea
Jorne Envy <jornejongsma@gmail.com>
2021-12-06 13:03:49 +0100

    update number

--- bin/cli.js
+++ bin/cli.js
@@ -14,9 +14,7 @@ function runCommand(command) {
 
 const repoName = process.argv[2];
 
-// This link should be public
-//const githubRepo = `https://github.com/jornejongsma/starter-app.git`;
-const githubRepo = ``;
+const githubRepo = `https://github.com/jornejongsma/create-app`;
 const gitCheckoutCommand = `git clone --depth 1 ${githubRepo} ${repoName}`;
 const installDepthCommand = `cd ${repoName} && yarn install`;
 
