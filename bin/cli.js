b83afcac8bc7632ae76d0b6585009e248847fdb2
Jorne Envy <jornejongsma@gmail.com>
2021-12-07 11:41:34 +0100

    reomve folders

--- bin/cli.js
+++ bin/cli.js
@@ -13,38 +13,57 @@ function runCommand(command) {
   return true;
 }
 
+function deleteFolder(location) {
+  try {
+    fs.rmdirSync(location, { recursive: true })
+  } catch (error) {
+    console.error(`Failed to remove ${location}`)
+    return false
+  }
+  return true
+}
+
 const repoName = process.argv[2];
 
 const githubRepo = `https://github.com/jornejongsma/create-app`;
 const gitCheckoutCommand = `git clone --depth 1 ${githubRepo} ${repoName}`;
 const installDepthCommand = `cd ${repoName} && yarn install`;
-
+const openRepoInVSCodeCommand = `code .\\${repoName}`
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
-if(!checkedOut) process.exit({code: -1});
+if(!checkedOut) process.exit(1);
 
 console.log(`Installing dependencies for ${repoName}`);
 const installedDeps = runCommand(installDepthCommand);
-if(!installedDeps) process.exit({code: -1});
+if(!installedDeps) process.exit(1);
 
-console.log(`clean up`);
+console.log(`Remove up bin and .github from ${repoName}`);
 const folder = process.cwd();
-const repoLocation = `${folder}\\${repoName}`
-console.log(`current location is: ${folder}\\${repoName}`)
-fs.readdir(repoLocation, (err, files) => {
-  if(err) {
-    console.log('error', err)
-    return err
-  }
-  files.forEach(file => {
-    console.log(file)
-  })
-})
+const repoLocation = `${folder}\\${repoName}`;
+const binLocation = `${repoLocation}\\bin`;
+const githubLocation = `${repoLocation}\\.github`;
 
-const openRepoInVSCodeCommand = `cd ${repoName} && code .`
+const deleteBin = deleteFolder(binLocation);
+if(!deleteBin) process.exit(1);
+const deleteGithub = deleteFolder(githubLocation);
+if(!deleteGithub) process.exit(1);
+
+
+// console.log(`current location is: ${folder}\\${repoName}`)
+// fs.readdir(repoLocation, (err, files) => {
+//   if(err) {
+//     console.log('error', err)
+//     return err
+//   }
+//   files.forEach(file => {
+//     console.log(file)
+//   })
+// })
+
+
 const openedVSCode = runCommand(openRepoInVSCodeCommand);
-if(!openedVSCode) process.exit({code: -1});
+if(!openedVSCode) process.exit(1);
 
-console.log("Congratulations! You are ready. Follow the following commands to start");
-console.log(`cd ${repoName} && npm start`);
+console.log("Congratulations, you are ready!");
+
