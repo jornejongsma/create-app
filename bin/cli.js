2d01067abfeb16b194ebdcdd10679ce95763bc80
Jorne Envy <jornejongsma@gmail.com>
2021-12-09 17:53:07 +0100

    prompt

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 const { execSync } = require('child_process');
 const fs = require('fs');
+const readline = require('readline');
 
 function runCommand(command) {
   try {
@@ -33,66 +34,88 @@ function writeFile(location, data) {
   return true;
 }
 
-const repoName = process.argv[2];
+let repoName = process.argv[2];
 
-const githubRepo = `https://github.com/jornejongsma/create-app`;
-const gitCheckoutCommand = `git clone --depth 1 ${githubRepo} ${repoName}`;
-const installDepthCommand = `cd ${repoName} && yarn install --silent`;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log(`Cloning the repository with name ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) process.exit(1);
+if (!repoName) {
+  rl.question('Enter a repo-name:', (name) => {
+    repoName = name;
+    rl.close();
+  });
+} else {
+  runIstallation()
+}
 
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepthCommand);
-if (!installedDeps) process.exit(1);
+rl.on("close", () => {
+  runIstallation()
+  process.exit(0);
+})
 
-console.log(`Remove up bin and .github from ${repoName}`);
-const folder = process.cwd();
-const repoLocation = `${folder}\\${repoName}`;
-const binLocation = `${repoLocation}\\bin`;
-const githubLocation = `${repoLocation}\\.github`;
-const gitLocation = `${repoLocation}\\.git`;
-const packageLocation = `${repoLocation}\\package.json`;
-const rawPackage = fs.readFileSync(packageLocation);
-const packageData = JSON.parse(rawPackage);
 
-const { bin, publishConfig, ...newPackage } = packageData;
-newPackage['name'] = repoName;
-newPackage['version'] = '0.1.0';
-newPackage['private'] = true;
+function runIstallation() {
+  const githubRepo = `https://github.com/jornejongsma/create-app`;
+  const gitCheckoutCommand = `git clone --depth 1 ${githubRepo} ${repoName}`;
+  const installDepthCommand = `cd ${repoName} && yarn install --silent`;
 
-const newRawPackage = JSON.stringify(newPackage, null, 2);
-const writePackage = writeFile(packageLocation, newRawPackage);
-if (!writePackage) process.exit(1);
+  console.log(`Cloning the repository with name ${repoName}`);
+  const checkedOut = runCommand(gitCheckoutCommand);
+  if (!checkedOut) process.exit(1);
 
-const workspaceData = {
-  folders: [
-    {
-      path: '.',
-    },
-  ],
-  settings: {},
-};
+  console.log(`Installing dependencies for ${repoName}`);
+  const installedDeps = runCommand(installDepthCommand);
+  if (!installedDeps) process.exit(1);
 
-const workspaceLocation  = `${repoLocation}\\${repoName}.code-workspace`
-const rawWorkspace = JSON.stringify(workspaceData, null, 2);
-const writeWorkspace = writeFile(workspaceLocation, rawWorkspace)
-if (!writeWorkspace) process.exit(1);
+  console.log(`Remove up bin and .github from ${repoName}`);
+  const folder = process.cwd();
+  const repoLocation = `${folder}\\${repoName}`;
+  const binLocation = `${repoLocation}\\bin`;
+  const githubLocation = `${repoLocation}\\.github`;
+  const gitLocation = `${repoLocation}\\.git`;
+  const packageLocation = `${repoLocation}\\package.json`;
+  const rawPackage = fs.readFileSync(packageLocation);
+  const packageData = JSON.parse(rawPackage);
 
-const deleteBin = deleteFolder(binLocation);
-if (!deleteBin) process.exit(1);
-const deleteGithub = deleteFolder(githubLocation);
-if (!deleteGithub) process.exit(1);
-const deleteGit = deleteFolder(gitLocation);
-if (!deleteGit) process.exit(1);
+  const { bin, publishConfig, ...newPackage } = packageData;
+  newPackage['name'] = repoName;
+  newPackage['version'] = '0.1.0';
+  newPackage['private'] = true;
 
-const gitInit = `git init`;
-const gitAddAll = `git add .`;
-const gitCommit = `git commit -q -m "first commit"`;
-const gitBranch = `git branch -M main`;
-const startGitCommand = `cd ${repoName} && ${gitInit} && ${gitAddAll} && ${gitCommit} && ${gitBranch}`;
-const startGit = runCommand(startGitCommand);
-if (!startGit) process.exit(1);
+  const newRawPackage = JSON.stringify(newPackage, null, 2);
+  const writePackage = writeFile(packageLocation, newRawPackage);
+  if (!writePackage) process.exit(1);
 
-console.log('Congratulations, you are ready!');
+  const workspaceData = {
+    folders: [
+      {
+        path: '.',
+      },
+    ],
+    settings: {},
+  };
+
+  const workspaceLocation = `${repoLocation}\\${repoName}.code-workspace`;
+  const rawWorkspace = JSON.stringify(workspaceData, null, 2);
+  const writeWorkspace = writeFile(workspaceLocation, rawWorkspace);
+  if (!writeWorkspace) process.exit(1);
+
+  const deleteBin = deleteFolder(binLocation);
+  if (!deleteBin) process.exit(1);
+  const deleteGithub = deleteFolder(githubLocation);
+  if (!deleteGithub) process.exit(1);
+  const deleteGit = deleteFolder(gitLocation);
+  if (!deleteGit) process.exit(1);
+
+  const gitInit = `git init`;
+  const gitAddAll = `git add .`;
+  const gitCommit = `git commit -q -m "first commit"`;
+  const gitBranch = `git branch -M main`;
+  const startGitCommand = `cd ${repoName} && ${gitInit} && ${gitAddAll} && ${gitCommit} && ${gitBranch}`;
+  const startGit = runCommand(startGitCommand);
+  if (!startGit) process.exit(1);
+
+  console.log('Congratulations, you are ready!');
+}
